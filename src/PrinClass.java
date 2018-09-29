2b7a1b974a11c230fcb364e59883b3e686ea8b79
oleguka <olrgolrg@yandex.ru>
2018-09-29 08:16:35 +0500

    test

--- src/PrinClass.java
+++ src/PrinClass.java
@@ -3,6 +3,6 @@ public class PrinClass {
         int i = 2;
         double r = Math.sqrt(i);
 
-        System.out.format("The square root of %d is %f.%n", i, r);
+        System.out.format("The square root of %d is %f.%n", i, r);//test
     }
 }
